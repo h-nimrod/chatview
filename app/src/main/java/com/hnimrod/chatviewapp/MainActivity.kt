5c05b6295167f7e4519f55ac138bfb42d397cf05
h-nimrod <h.tanabe@gmail.com>
2019-03-16 21:43:48 +0900

    default settings

---
+++ app/src/main/java/com/hnimrod/chatviewapp/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.hnimrod.chatviewapp
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
